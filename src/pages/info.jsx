b54222d0daea83cc8f0c184329f7466a7c2a753c
John Doe <johndoe@example.com>
2025-05-31 19:47:19 +0300

    Add new buttons on mainPage

--- src/pages/info.jsx
+++ src/pages/info.jsx
@@ -1,10 +1,11 @@
-import { AddBoxRounded } from "@mui/icons-material";
 import { Container, Box, Typography, Button } from "@mui/material";
-import tireImage from '../assets/tire4.webp'
 import './image.css'
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Info = () => {
+
+    const navigate = useNavigate('')
+
     return (
         <Container sx={{mt: '40px', display: 'flex', pb: '50px' }}>
             {/* Основной контейнер с красным фоном */}
@@ -12,7 +13,7 @@ const Info = () => {
                 {/* Контейнер с синим фоном, который занимает всю ширину */}
                 <Box sx={{ backgroundColor: '#080A77', borderRadius: '35px', mb: '25px',height: '400px', width: '850px', transition: 'transform 0.3s', '&:hover': { transform: 'scale(1.03)' } }}>
                     <div className="image-container">
-                        <Box sx={{padding: '50px', height: '400px'}}>
+                        <Box sx={{padding: '50px', height: '300px'}}>
                             <Typography sx={{fontSize: '32px', color: '#CD2311'}}>
                                 Выберите подходящие
                             </Typography>
@@ -32,11 +33,14 @@ const Info = () => {
                 </Box>
                 {/* Контейнер для двух карточек с зеленым и красным фоном */}
                 <Box sx={{ display: 'flex' }}>
-                    <Box 
+                    <Box className='background-gradient' onClick={() => navigate('/delivery')}
                         sx={{ 
                             backgroundColor: '#A8F000',
                             width: '412.5px',  
-                            height: '100px', 
+                            height: '100px',
+                            cursor: 'pointer',
+                            display: 'flex',
+                            alignItems: 'center', 
                             borderRadius: '25px', 
                             mr: '25px', 
                             transition: 'transform 0.3s', 
@@ -45,11 +49,20 @@ const Info = () => {
                             }
                         }}
                     >
+                        <Typography sx={{ml: '40px', fontSize: '24px', color: 'white'}}>
+                            Транспартировка
+                        </Typography>
+                        <div className="delivery-image">
+
+                        </div>
                     </Box>
-                    <Box 
+                    <Box className='background-gradient2' onClick={() => navigate('/guarantee')}
                         sx={{ 
                             backgroundColor: '#EF3123',
-                            width: '412.5px',  
+                            width: '412.5px',
+                            cursor: 'pointer',
+                            display: 'flex',
+                            alignItems: 'center',  
                             height: '100px', 
                             borderRadius: '25px', 
                             transition: 'transform 0.3s', 
@@ -58,14 +71,26 @@ const Info = () => {
                             }
                         }}
                     >
+                        <Typography sx={{ml: '40px', fontSize: '24px', color: 'white'}}>
+                            Гарантия
+                        </Typography>
+                        <div className="guarantee-image">
+
+                        </div>
                     </Box>
                 </Box>
             </Box>
             {/* Второй контейнер с белым фоном */}
             <Box sx={{ height: '500px', ml: '25px' }}>
                 <Box sx={{ backgroundColor: '#9EDFFF', width: '270px', height: '250px', mb: '25px', borderRadius: '35px', transition: 'transform 0.3s', '&:hover': { transform: 'scale(1.05)' } }}>
+                       <div className="sale-image" onClick={() => navigate('/sale')} >
+        
+                        </div>
                 </Box>
                 <Box sx={{ backgroundColor: '#B3CCFF', width: '270px', height: '250px', borderRadius: '35px', transition: 'transform 0.3s', '&:hover': { transform: 'scale(1.05)' } }}>
+                        <div className="navigation-image" onClick={() => navigate('/navigation')}>
+                            
+                        </div>
                 </Box>
             </Box>
             
