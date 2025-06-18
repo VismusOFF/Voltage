5ed52215bfa600f39f0e71f6dab91eb449447e92
John Doe <johndoe@example.com>
2025-06-18 12:43:45 +0300

    fix icon map

--- src/pages/navigation/navigation.jsx
+++ src/pages/navigation/navigation.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Container, Box } from "@mui/material";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet'; 
 import 'leaflet/dist/leaflet.css';
 
+import customIconUrl from '../../assets/location.png';
+
 const NavigationComponent = () => {
-    const position = [41.732317, 44.779525]; // Замените на координаты вашего магазина
+    const position = [41.732317, 44.779525];
+
+    const customIcon = new L.Icon({
+        iconUrl: customIconUrl,
+        iconSize: [40, 40], 
+        iconAnchor: [20, 40],
+        popupAnchor: [0, -40],
+        shadowUrl: null,
+        shadowSize: null,
+    });
 
     return (
         <Container sx={{ mt: '100px', mb: '100px' }}>
@@ -14,7 +26,7 @@ const NavigationComponent = () => {
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     />
-                    <Marker position={position}>
+                    <Marker position={position} icon={customIcon}>
                         <Popup>Местонахождение магазина</Popup>
                     </Marker>
                 </MapContainer>
